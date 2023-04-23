a9064d64f7760aba20065522891ac15530d14d42
jeremieflrnt <55829284+jeremieflrnt@users.noreply.github.com>
2023-04-23 21:45:04 +0200

    color darkmode missing (#7)
    Co-authored-by: jeremieflrnt <jeremie.fleurant@gmail.com>

--- components/weightlifting/Calculator.tsx
+++ components/weightlifting/Calculator.tsx
@@ -9,28 +9,28 @@ const Calculator = (props: Props) => {
       <div className="stats-like grid grid-flow-row grid-cols-[auto_auto] gap-3 p-2 shadow sm:grid-cols-3 lg:grid-cols-5">
         <div className="flex flex-col">
           <div className="text-xs ">90%</div>
-          <div className="flex items-baseline font-extrabold text-neutral">
+          <div className="flex items-baseline font-extrabold">
             <div className="text-lg">{(props.weight * 0.9).toFixed().toString()}</div>
             <div className="text-xs">kg</div>
           </div>
         </div>
         <div className="flex flex-col">
           <div className="text-xs ">80%</div>
-          <div className="flex items-baseline font-extrabold text-neutral">
+          <div className="flex items-baseline font-extrabold">
             <div className="text-lg">{(props.weight * 0.8).toFixed().toString()}</div>
             <div className="text-xs">kg</div>
           </div>
         </div>
         <div className="flex flex-col">
           <div className="text-xs ">70%</div>
-          <div className="flex items-baseline font-extrabold text-neutral">
+          <div className="flex items-baseline font-extrabold">
             <div className="text-lg">{(props.weight * 0.7).toFixed().toString()}</div>
             <div className="text-xs">kg</div>
           </div>
         </div>
         <div className="flex flex-col">
           <div className="text-xs ">60%</div>
-          <div className="flex items-baseline font-extrabold text-neutral">
+          <div className="flex items-baseline font-extrabold">
             <div className="text-lg">{(props.weight * 0.6).toFixed().toString()}</div>
             <div className="text-xs">kg</div>
           </div>
